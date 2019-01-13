e5c3fc431199076fe546ebaaf1b999a088386eb9
Andrew N <34643231+Bopobywek@users.noreply.github.com>
2019-01-13 15:57:26 +0300

    Added model

--- predict_emotion.py
+++ predict_emotion.py
@@ -6,7 +6,7 @@ results = []
 classifier2 = CatBoostClassifier(iterations=1000, learning_rate=0.25,
                                  depth=5, loss_function='MultiClassOneVsAll',
                                  eval_metric="Accuracy")
-classifier2.load_model("stabel_model")
+classifier2.load_model("stable_model")
 data = At.dirsWavFeatureExtraction(["data/"], 1, 1, 0.05, 0.05)
 for element in data[0]:
     result = classifier2.predict(element)
